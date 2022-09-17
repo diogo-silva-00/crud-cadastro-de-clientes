3698dddf2676716acba87fed453119c05ef08bed
Dg <dgtank9@gmail.com>
2022-09-17 16:29:39 -0300

    Alteração de nome para TopBar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { TopbarComponent } from './components/topbar/topbar.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material/material.module';
 import { TelaCadastroComponent } from './components/tela-cadastro/tela-cadastro.component';
@@ -11,7 +11,7 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome'
 @NgModule({
   declarations: [
     AppComponent,
-    ToolbarComponent,
+    TopbarComponent,
     TelaCadastroComponent
   ],
   imports: [
